f4abded7d944868560489451580e7237eff629ff
richmondn <20654625+richmondn@users.noreply.github.com>
2024-07-19 19:31:46 -0500

    feat: Update Azure credentials configuration script
    The PowerShell script (config_gh.ps1) for configuring Azure credentials has been updated. The script now correctly sets the ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, and ARM_TENANT_ID secrets using the service principal's clientId, clientSecret, subscriptionId, and tenantId respectively. This ensures that the MLOps project has the necessary credentials for authentication with Azure.

--- config_gh.ps1
+++ config_gh.ps1
@@ -1,16 +1,13 @@
 $project_name = 'mlops-project1'
-$github_org_name = 'richmondn'
 $spn_name = 'mlops-project1-sp'
-$subscription_id = (az account show --query id -o tsv)
 
-$spn_json = az ad sp create-for-rbac --name $spn_name --role Contributor --scopes /subscriptions/$subscription_id
+$spn_json = az ad sp create-for-rbac --name $spn_name --role Contributor --scopes /subscriptions/$subscription_id --json-auth
 $spn = $spn_json | ConvertFrom-Json -AsHashtable
 
 Set-Location ../$project_name
 
 gh secret set AZURE_CREDENTIALS -b "$spn_json"
-gh secret set ARM_CLIENT_ID -b "$($spn.appId)"
-gh secret set ARM_CLIENT_SECRET -b "$($spn.password)"
-gh secret set ARM_SUBSCRIPTION_ID -b "$subscription_id"
-gh secret set ARM_TENANT_ID -b "$($spn.tenant)"
-
+gh secret set ARM_CLIENT_ID -b "$($spn.clientId)"
+gh secret set ARM_CLIENT_SECRET -b "$($spn.clientSecret)"
+gh secret set ARM_SUBSCRIPTION_ID -b "$($spn.subscriptionId)"
+gh secret set ARM_TENANT_ID -b "$($spn.tenantId)"
